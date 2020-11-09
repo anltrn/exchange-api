d05c8a4d22fd9602120d07dffa4a96333731655f
Anıl Turan <anil.trn45@gmail.com>
2020-11-09 08:16:30 +0300

    swagger added.

--- src/main/java/com/anil/turan/exchange/api/service/impl/ExchangeRateImpl.java
+++ src/main/java/com/anil/turan/exchange/api/service/impl/ExchangeRateImpl.java
@@ -53,7 +53,6 @@ public class ExchangeRateImpl implements ExchangeRateService {
 	    headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 	    ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-	    //System.out.println(res);
 	    Gson gson= new Gson();
 	    ResponseExchangeRateDTO exchangeDTO = gson.fromJson(res.getBody(), ResponseExchangeRateDTO.class);
 	    Double currencyRate = exchangeDTO.getRates().get(targetCurrency);
